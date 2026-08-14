8d5bfb7d78570463bed39d9e97f371e263c73946
KatojiRimu <katojirimu@gmail.com>
2026-08-15 01:39:30 +0700

    Update fetchReviews/dealer endpoint path for Question 8 Coursera rubric

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,11 +14,15 @@ urlpatterns = [
     
     # Dealers endpoints
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='fetchDealers', view=views.get_dealerships, name='fetch_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='fetchDealers/<str:state>', view=views.get_dealerships, name='fetch_dealers_by_state'),
     path(route='get_dealer/<int:dealer_id>', view=views.get_dealer_details, name='get_dealer_by_id'),
+    path(route='fetchDealer/<int:dealer_id>', view=views.get_dealer_details, name='fetch_dealer_by_id'),
     
     # Reviews & Sentiment endpoints
     path(route='get_reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='get_dealer_reviews'),
+    path(route='fetchReviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='fetch_dealer_reviews'),
     path(route='add_review', view=views.add_review, name='add_review'),
     path(route='analyze/<str:text>', view=views.analyze_review_sentiment, name='analyze_review'),
 ]
